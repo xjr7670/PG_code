ed311547e60edd6cf82fcf6df264ff36951baa02
Cavin <xian.jr@pg.com>
2017-10-27 13:34:39 +0800

    no change

--- sap_zedr_htm.py
+++ sap_zedr_htm.py
@@ -38,10 +38,9 @@ def get_data(fname, code_list):
         result_string += str(result_dict[d][code_list[2]]["dc"]) + "," + str(result_dict[d][code_list[2]]["intransit"]) + ","
         result_string += str(result_dict[d][code_list[3]]["dc"]) + "," + str(result_dict[d][code_list[3]]["intransit"]) + ","
         result_string += str(result_dict[d][code_list[4]]["dc"]) + "," + str(result_dict[d][code_list[4]]["intransit"]) + ","
-        result_string += str(result_dict[d][code_list[5]]["dc"]) + "," + str(result_dict[d][code_list[5]]["intransit"])
+        result_string += str(result_dict[d][code_list[5]]["dc"]) + "," + str(result_dict[d][code_list[5]]["intransit"]) + ","
         print(result_string)
 
 koala_code = ["82261753", "82261756", "82261762", "82261951", "82263780", "82264483"]
-tampax_code = ["82255634", "82258740", "82259122", "82258746", "82258747", "82261076"]
 get_data("ka.htm", koala_code)
 # get_data("ta.htm", tampax_code)
